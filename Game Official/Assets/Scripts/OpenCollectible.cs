74f236291510245eb414439341527260a936363f
tricksterkid <deighton.zakelijk@gmail.com>
2020-03-06 20:02:59 +0100

    VERSION 4 - Added first models (no texture), added collectible script.

---
+++ Game Official/Assets/Scripts/OpenCollectible.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class OpenCollectible : MonoBehaviour
+{
+
+    public GameObject Panel;
+    public GameObject Trigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Panel.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Panel.SetActive(false);
+            Time.timeScale = 1.0f;
+        }
+    }
+    
+}
--- Game Official/Assets/Scripts/StartGame.cs
+++ Game Official/Assets/Scripts/StartGame.cs
@@ -2,22 +2,11 @@
 using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
-{
-    private Animation fadeOut;
-
-    void Start()
-    {
-        fadeOut = gameObject.GetComponent<Animation>();
-    }
+{ 
 
     public void Begin()
     {
         SceneManager.LoadScene("Instructions");
-    }
-
-    public void Faded()
-    {
-        fadeOut.Play("FadeOut");
     }
 
 }
