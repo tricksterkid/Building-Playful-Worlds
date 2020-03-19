91b81cdcd6dfeccd9ea46e9297bb7fe035e9b0ad
tricksterkid <deighton.zakelijk@gmail.com>
2020-03-19 17:23:44 +0100

    VERSION 11.2 - Fixed part of box glitch, cleaned up workspace part 2, finished decorating another room.

--- Game Official/Assets/Scripts/StartCutscene.cs
+++ Game Official/Assets/Scripts/StartCutscene.cs
@@ -9,38 +9,46 @@ public class StartCutscene : MonoBehaviour
     // add in box to make sure box is always closed, even when cutscene is skipped
     public GameObject Box;
     private Animation closeBox;
+    private bool IsCutscenePlaying = false;
 
-    private void Start()
+    // get animation
+    void Start()
     {
         closeBox = Box.GetComponent<Animation>();
     }
 
+    // what happens when player walks into trigger
     void OnTriggerEnter(Collider other)
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
         CutsceneCam.SetActive(true);
         SkipScene.SetActive(true);
         Player.SetActive(false);
+        IsCutscenePlaying = true;
         StartCoroutine(FinishCut());
         StartCoroutine(DeleteMessage());
     }
 
+    // coroutine that manages cutscene
     IEnumerator FinishCut()
     {
         yield return new WaitForSeconds(30);
         Player.SetActive(true);
         CutsceneCam.SetActive(false);
+        IsCutscenePlaying = false;
     }
 
+    // coroutine that manages the skip cutscene message
     IEnumerator DeleteMessage()
     {
         yield return new WaitForSeconds(5);
         SkipScene.SetActive(false);
     }
 
+    // what happens when the player presses e when the cutscene is playing
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (IsCutscenePlaying == true && Input.GetKeyDown(KeyCode.E))
         {
             Player.SetActive(true);
             closeBox.Play("Chest Closes");
