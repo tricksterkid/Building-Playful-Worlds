548cb41177b67bd572c078c360dfeafd3de2b3ff
tricksterkid <deighton.zakelijk@gmail.com>
2020-03-23 13:02:51 +0100

    VERSION 11.6 - Added collectible text, added pause menu, added blood stain for environmental storytelling. MISSING: cutscene elements.

---
+++ Game Official/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    bool IsGamePaused = false;
+    public GameObject PauseScreen;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+            {
+            PauseScreen.SetActive(true);
+            IsGamePaused = true;
+            Time.timeScale = 0.0f;
+        }
+
+        if (IsGamePaused == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                PauseScreen.SetActive(false);
+                IsGamePaused = false;
+                Time.timeScale = 1.0f;
+            }
+        }
+    }
+}
