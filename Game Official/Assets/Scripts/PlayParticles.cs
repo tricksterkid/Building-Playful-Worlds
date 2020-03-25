61b3e5432a513a107b75bb9394c05c36283d74a5
tricksterkid <deighton.zakelijk@gmail.com>
2020-03-25 10:54:19 +0100

    VERSION 11.8 - Added particle system to cutscene.

---
+++ Game Official/Assets/Scripts/PlayParticles.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayParticles : MonoBehaviour
+{
+    public GameObject Particles;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("CutsceneCam"))
+        {
+            Particles.SetActive(true);
+        }
+    }
+}
